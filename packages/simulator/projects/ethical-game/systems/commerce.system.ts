c943bb8adb565b2e61ea33c9a95eddae768b573a
Nozomu Miyamoto <irregular0number@gmail.com>
2021-05-26 01:21:24 +0900

    fix(projects): fix algorithm to calc success rate

--- packages/simulator/projects/ethical-game/systems/commerce.system.ts
+++ packages/simulator/projects/ethical-game/systems/commerce.system.ts
@@ -48,7 +48,6 @@ export class CommerceSystem extends BaseCommerceSystem {
     return (sellerW + buyerW) / (Math.min(buyerW, sellerW) * buyerT);
   }
 
-  // TODO: できれば逐次処理に変更
   // 最低信頼度Tを取得する
   private getMinimumTrustScore(t: number, playerId: PlayerId, excludeIds: PlayerId[]) {
     // 時刻tのTransactionがなければエラー
@@ -58,27 +57,11 @@ export class CommerceSystem extends BaseCommerceSystem {
     // 時刻t-1のbalancesを取得
     const balances = this.getBalances(t-1);
 
-    // 自己信頼
-    let trust = this.getReputationWeight(balances, playerId);
-
-    // TODO: 逐次処理に変更
-    // playerが過去に商取引ゲームを行った相手の一覧を作成する
-    const opportunityIds = new Set<PlayerId>();
-    for (const transaction of Object.values(this.store.getHistory())) {
-      if (transaction.sellerId === playerId) {
-        opportunityIds.add(transaction.buyerId)
-      } else if (transaction.buyerId === playerId) {
-        opportunityIds.add(transaction.sellerId)
-      }
-    }
-
     // 各opportunityとの商取引ゲームの成功率の荷重和をとる
-    for (const opportunityId of [...opportunityIds].filter(id => !excludeIds.includes(id))) {
-      const [successRate] = this.getSuccessRate(t, playerId, opportunityId);
-      trust += this.getReputationWeight(balances, opportunityId) * successRate;
-    }
+    return this.getPlayerIds(excludeIds).reduce((trust, id) => {
+      return trust + this.getReputationWeight(balances, id, excludeIds) * this.getSuccessRate(t, playerId, id)[0];
+    }, 0);
 
-    return trust;
   }
 
   // targetのプレイヤーのbalanceの割合を取得
@@ -92,6 +75,7 @@ export class CommerceSystem extends BaseCommerceSystem {
 
   // 過去の商取引ゲームで成功が報告された割合を取得
   getSuccessRate(t: number, playerId: PlayerId, opportunityId: PlayerId): [number, number] {
+    if (playerId === opportunityId) return [1, 0];
     if (t === 0) return [1/2, 0];
 
     // 時刻tのTransactionがなければエラー
@@ -100,10 +84,9 @@ export class CommerceSystem extends BaseCommerceSystem {
 
     const {sellerId, buyerId, result} = transaction;
     let [successRate, count] = this.getSuccessRate(t - 1, playerId, opportunityId);
-    successRate = count === 0 ? 0 : successRate;
-
     if (isSetsEqual(new Set([sellerId, buyerId]), new Set([playerId, opportunityId]))) {
-      return [(successRate * count + result === Result.SUCCESS ? 1 : 0) / (count + 1), count + 1];
+      successRate = count === 0 ? 0 : successRate;
+      return [(successRate * count + (result === Result.SUCCESS ? 1 : 0)) / (count + 1), count + 1];
     } else {
       return [successRate, count];
     }
